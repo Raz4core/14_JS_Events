180abf168ee28ab3cbff8cc01a99013a9c12a9b7
Raz4core <mail@test.de>
2022-01-26 12:54:10 +0100

    Modul: Update der View-Schicht

--- events.js
+++ events.js
@@ -10,8 +10,31 @@ btn.addEventListener("click",toggleAppStatus);
 
 function toggleAppStatus() {
     appStatus = !appStatus;
-    output(appStatus);
+    updateview();
 }
+/**** View-Schicht ****/
+
+// Modul: Update der View-Schicht
+function updateview() {
+    if (appStatus) {
+        switchClassName("day");
+    } else{
+        switchClassName("night");
+    }
+}
+
+// Modul: Umschaltung Klassennamen I Test:
+// switchClassName("night");
+// switchClassName("night");
+
+function switchClassName(modeStr) {
+
+    // Kritik: wenig Robust, da starre Indexierung
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
+
 
 
 /* Tools */
