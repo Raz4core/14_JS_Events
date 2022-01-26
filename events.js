7331f441ef35f0fa6115c2f11cb2d742c9d2950a
Raz4core <mail@test.de>
2022-01-26 12:26:37 +0100

    Beispiele Event-Listener

--- events.js
+++ events.js
@@ -1,15 +1,20 @@
 /** Variablen */
 
-//const btn = document.getElementById("trigBtn");
-const btn = document.body;
+const btn = document.getElementById("trigBtn");
+// const btn = document.body;
 output(btn);
 
 /** Event-Listener ***/
 
 btn.addEventListener("click",test);
+btn.addEventListener("mouseenter",testEnter);
 
 function test() {
     output("Btn clicked!");
+}
+
+function testEnter() {
+    output("Mouse entered");
 }
 
 /* Tools */
